65884db2b8dce0b785ae7c6ec642e73a2ef00da2
David Milai <davidmilai67@gmail.com>
2022-10-15 18:03:43 +0300

    minting nfts

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,4 +3,13 @@
 pragma solidity ^0.8.9;
 
 // import "hardhat/console.sol";
-contract NFT{}
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721{
+
+
+    constructor() ERC721("Learn NFT","L_NFT"){
+        _mint(msg.sender,10*(10**8));
+    }
+}
