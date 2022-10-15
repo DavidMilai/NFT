8e1f327b657cbf6104e0f9ce07649aca1b91f15c
David Milai <davidmilai67@gmail.com>
2022-10-15 20:54:25 +0300

    hardhard config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+const { network } = require('hardhat');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+
+
+networks:{
+  goerli: {
+    url: process.env.ALCHEMY_API_KEY_URL,
+    accounts: [process.env.PRIVATE_KEY],
+  },
+} 
+
 };
