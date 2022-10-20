9e7dd95b13854d529d730542cc616b900d7551a7
David Milai <davidmilai67@gmail.com>
2022-10-20 07:05:51 +0300

    Inheritance

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract NFT is ERC721{
 
 
-    constructor() ERC721("Mil NFT Collection","MNC"){
+    constructor() ERC721("Cool NFT Collection","CNC"){
         _mint(msg.sender,1);
         _mint(msg.sender,2);
         _mint(msg.sender,3);
